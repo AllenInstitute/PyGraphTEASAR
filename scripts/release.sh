26464bb652e085faa6727d9e60b19ce96ada2e08
Forrest Collman <forrest.collman@gmail.com>
2023-04-03 14:01:36 -0700

    fix release typo

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,4 +4,4 @@ git push && git push --tags
 TAG=$(git describe --tags)
 poetry build
 twine upload dist/graphteasar-${TAG:1}.tar.gz
-twine upload dist/graphteasar-${TAG:1}-py3-non-any.whl
+twine upload dist/graphteasar-${TAG:1}-py3-none-any.whl
